257b08149a24ea2e792dc33ba4b2b06ce67e056c
kit <kit.mavareik@gmail.com>
2023-03-07 18:21:33 +0200

    Introducing redux toolkit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,22 +1,31 @@
+// MODULES
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {ThemeProvider} from "@emotion/react";
+import {Global} from "@emotion/react";
+import {Provider} from 'react-redux'
+
+// RESOURCES
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Global} from "@emotion/react";
+import {store} from "src/store/store";
+
+// STYLES
 import {globalStyles} from "src/styles/global.styles";
-import {ThemeProvider} from "@emotion/react";
 import {theme} from "src/styles/theme.style";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-      <ThemeProvider theme={theme}>
-      <Global styles={globalStyles} />
-        <App />
-      </ThemeProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <Global styles={globalStyles}/>
+                <App/>
+            </ThemeProvider>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
