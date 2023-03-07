3ba2096f5c14f3f424528ab8344b909bfc239e1a
kit <kit.mavareik@gmail.com>
2023-03-07 18:22:52 +0200

    Introducing filter state in app

--- client/src/components/filter-button/filter-button.tsx
+++ client/src/components/filter-button/filter-button.tsx
@@ -1,37 +1,44 @@
 /** @jsxImportSource @emotion/react */
 // TODO: resolve
 
+// MODULES
+import {useCallback, useMemo} from "react";
+import {useAppDispatch, useAppSelector} from "src/store/hooks";
+
 //COMPONENTS
 import {ReactComponent as EnabledIcon} from "src/resources/filter_enabled.svg";
 import {ReactComponent as DisabledIcon} from "src/resources/filter_disabled.svg";
 
+// RESOURCES
+import {enable, disable, selectFilterState} from "src/store/filter/filter.slice";
+
 // STYLE
 import {style} from 'src/components/filter-button/filter-button.style';
-import {useCallback, useMemo, useState} from "react";
 
 /**
  *
  */
 export const FilterButton = () => {
-    const [enabled, setEnabled] = useState(false);
+    const filterEnabled = useAppSelector(selectFilterState);
+    const dispatch = useAppDispatch();
 
     const handleEnableFilter = useCallback(() => {
-        setEnabled(true);
+        dispatch(enable());
         console.log('Filter enabled');
     }, []);
 
     const handleDisableFilter = useCallback(() => {
-        setEnabled(false);
+        dispatch(disable());
         console.log('Filter disabled');
     }, []);
 
     const renderIcon = useMemo(() => {
         return (
-            <button css={style} onClick={(enabled) ? handleDisableFilter : handleEnableFilter}>
-                {(enabled) ? <EnabledIcon/> : <DisabledIcon/>}
+            <button css={style} onClick={(filterEnabled) ? handleDisableFilter : handleEnableFilter}>
+                {(filterEnabled) ? <EnabledIcon/> : <DisabledIcon/>}
             </button>
         )
-    }, [enabled]);
+    }, [filterEnabled]);
 
     return renderIcon;
 };
--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -4,6 +4,10 @@
 //COMPONENTS
 import {Typography} from "src/components/typography/typography";
 
+// RESOURCES
+import {useAppSelector} from "src/store/hooks";
+import {selectFilterState} from "src/store/filter/filter.slice";
+
 // STYLE
 import { style } from 'src/components/footer/footer.style';
 
@@ -11,9 +15,12 @@ import { style } from 'src/components/footer/footer.style';
  *
  */
 export const Footer = () => {
+    const filterEnabled = useAppSelector(selectFilterState);
+
     return (
             <footer css={style}>
                 <Typography variant='Text'>Sensors available: 6</Typography>
+                <Typography variant='Text'>Filter: {(filterEnabled) ? 'enabled' : 'disabled'}</Typography>
                 <Typography variant='Text'>Showing: 6</Typography>
             </footer>
     );
