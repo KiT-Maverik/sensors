e3338ac428cc1e5805c29ddc00e1e88da50cc00a
kit <kit.mavareik@gmail.com>
2023-03-07 18:23:14 +0200

    Label restyling

--- client/src/components/label/label.style.ts
+++ client/src/components/label/label.style.ts
@@ -5,17 +5,14 @@ const container: Interpolation<Theme> = ({ spacing }: Theme) => ({
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
-    gap: spacing.l,
+    gap: spacing.s / 2,
     width: '100%',
     padding: `0 ${spacing.s / 2}px` ,
     boxSizing: 'border-box',
 });
 
-const sensorIcon: Interpolation<Theme> = ({ color, spacing }: Theme) => ({
-    display: 'flex',
-    justifyContent: 'space-between',
-    gap: spacing.l,
-    fill: color.secondary,
+const sensorIcon: Interpolation<Theme> = ({ color }: Theme) => ({
+    fill: color.text,
 });
 
 const header: Interpolation<Theme> = ({ color }: Theme) => ({
