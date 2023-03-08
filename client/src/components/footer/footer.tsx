7b306de32654eeee13490debc4fe1318d0f6dd4a
kit <kit.mavareik@gmail.com>
2023-03-08 05:28:55 +0200

    Removing extra logic

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -1,30 +1,24 @@
 /** @jsxImportSource @emotion/react */
 
-//COMPONENTS
+// COMPONENTS
 import {Typography} from "src/components/typography/typography";
 
 // RESOURCES
 import {useAppSelector} from "src/store/hooks";
 import {selectFilterState} from "src/store/filter/filter.slice";
-import {selectAvailableSensorsState} from "src/store/filter/available-sensors.slice";
 
 // STYLE
 import { style } from 'src/components/footer/footer.style';
-import {selectDisplayedSensorsState} from "src/store/filter/displayed-sensors.slice";
 
 /**
  *
  */
 export const Footer = () => {
-    const availableSensors = useAppSelector(selectAvailableSensorsState);
-    const displayedSensors = useAppSelector(selectDisplayedSensorsState);
     const filterEnabled = useAppSelector(selectFilterState);
 
     return (
             <footer css={style}>
-                <Typography variant='Text'>Sensors available: {availableSensors}</Typography>
                 <Typography variant='Text'>Filter: {(filterEnabled) ? 'enabled' : 'disabled'}</Typography>
-                <Typography variant='Text'>Showing: {displayedSensors}</Typography>
             </footer>
     );
 };
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -3,15 +3,11 @@ import {configureStore, combineReducers} from '@reduxjs/toolkit'
 
 // REDUCERS
 import {filterReducer} from "src/store/filter/filter.slice";
-import {availableSensorsReducer} from "src/store/filter/available-sensors.slice";
-import {displayedSensorsReducer} from "src/store/filter/displayed-sensors.slice";
 
 export const store = configureStore({
     reducer: {
         filter: combineReducers({
             status: filterReducer,
-            availableSensors: availableSensorsReducer,
-            displayedSensors: displayedSensorsReducer,
         }),
     }
 });
