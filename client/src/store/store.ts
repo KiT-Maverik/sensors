440a8246f0f3ae0938c04fcf3294617a08a50d1c
kit <kit.mavareik@gmail.com>
2023-03-08 04:15:44 +0200

    Removing extra slices

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -5,12 +5,6 @@ import {configureStore, combineReducers} from '@reduxjs/toolkit'
 import {filterReducer} from "src/store/filter/filter.slice";
 import {availableSensorsReducer} from "src/store/filter/available-sensors.slice";
 import {displayedSensorsReducer} from "src/store/filter/displayed-sensors.slice";
-import {humidityReducer} from "src/store/sensors/humidity.slice";
-import {pm10Reducer} from "src/store/sensors/pm10.slice";
-import {pm25Reducer} from "src/store/sensors/pm25.slice";
-import {pressureReducer} from "src/store/sensors/pressure.slice";
-import {temperatureReducer} from "src/store/sensors/temperature.slice";
-import {windReducer} from "src/store/sensors/wind.slice";
 
 export const store = configureStore({
     reducer: {
@@ -18,14 +12,6 @@ export const store = configureStore({
             status: filterReducer,
             availableSensors: availableSensorsReducer,
             displayedSensors: displayedSensorsReducer,
-        }),
-        sensors: combineReducers({
-            humidity: humidityReducer,
-            pm10: pm10Reducer,
-            pm25: pm25Reducer,
-            pressure: pressureReducer,
-            temperature: temperatureReducer,
-            wind: windReducer,
         }),
     }
 });
